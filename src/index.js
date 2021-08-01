23148a393934ea9e268437b77ff87ddbd50bad31
avoysarkar <73129752+avoysarkar@users.noreply.github.com>
2021-08-01 22:45:32 +0530

    Update wiht ES6 code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Avoy";
+const lname = "Sarkar";
+const num = 429;
+
+ReactDOM.render(
+  <div>
+    {/* Below show ES6 code  */}
+    <h1>Hello {`${fname} ${lname}`}</h1>
+    <p>My Roll Number was UGCS16{num}</p>
+    <p>My Random number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
